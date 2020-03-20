b02d55ea262db1d5c4fce7972c54a4ebe5f2eda0
Marcel <marcel.vigneault.1420@gmail.com>
2020-03-19 21:40:05 -0400

    Removed the different types of questions.

--- games/charley.js
+++ games/charley.js
@@ -2,71 +2,86 @@ var players = [];
 var started = false;
 var paused = false;
 var channelID = '';
-var answer = 0;
+var answer = null;
 var idInterval = null;
 var { randomize } = require('../helpers/math');
 var gameActions = require('./charley_actions');
 function startGame(bot) {
     idInterval = setInterval(() => {
         if (!paused && started && players.length > 0) {
-            if (randomize(4) === 1) {
-                let currentGame = randomize(2);
+            let currPlayer = players[randomize(players.length) - 1];
+            let currentGame = randomize(6);
+            let actionString = '';
+            switch (currentGame) {
+                case 1:
+                    actionString = gameActions.giveSips();
+                    bot.channels.cache
+                        .get(channelID)
+                        .send(`<@${currPlayer}>, ${actionString}.`);
+                    break;
 
-                switch (currentGame) {
-                    case 1:
-                        bot.channels.cache
-                            .get(channelID)
-                            .send(`Everyone drink one sip !`);
-                        break;
+                case 2:
+                    actionString = gameActions.takeSips();
+                    bot.channels.cache
+                        .get(channelID)
+                        .send(`<@${currPlayer}>, ${actionString}.`);
+                    break;
 
-                    case 2:
-                        let number1 = randomize(12);
-                        let number2 = randomize(12);
-                        answer = number1 * number2;
-                        paused = true;
-                        bot.channels.cache
-                            .get(channelID)
-                            .send(
-                                `Answer this question: Calculate ${number1}x${number2}`
-                            );
-                        break;
-                }
-            } else {
-                let currPlayer = players[randomize(players.length) - 1];
-                let currentGame = randomize(7);
-                let actionString = '';
-                switch (currentGame) {
-                    case 1:
-                        actionString = gameActions.giveSips();
-                        break;
+                case 3:
+                    actionString = gameActions.shareSips();
+                    bot.channels.cache
+                        .get(channelID)
+                        .send(`<@${currPlayer}>, ${actionString}.`);
+                    break;
 
-                    case 2:
-                        actionString = gameActions.takeSips();
-                        break;
+                case 4:
+                    actionString = gameActions.giveSips(players);
+                    bot.channels.cache
+                        .get(channelID)
+                        .send(`<@${currPlayer}>, ${actionString}.`);
+                    break;
 
-                    case 3:
-                        actionString = gameActions.shareSips();
-                        break;
+                case 5:
+                    actionString = gameActions.shareSips(players);
+                    bot.channels.cache
+                        .get(channelID)
+                        .send(`<@${currPlayer}>, ${actionString}.`);
+                    break;
 
-                    case 4:
-                        actionString = gameActions.giveSips(players);
-                        break;
+                case 6:
+                    actionString = gameActions.category();
+                    bot.channels.cache
+                        .get(channelID)
+                        .send(`<@${currPlayer}>, ${actionString}.`);
+                    paused = true;
+                case 7:
+                    bot.channels.cache
+                        .get(channelID)
+                        .send(`Everyone drink one sip !`);
+                    break;
 
-                    case 5:
-                        actionString = gameActions.shareSips(players);
-                        break;
+                case 8:
+                    let number1 = randomize(12);
+                    let number2 = randomize(12);
+                    answer = number1 * number2;
+                    paused = true;
+                    bot.channels.cache
+                        .get(channelID)
+                        .send(
+                            `Answer this question: Calculate ${number1}x${number2}.`
+                        );
+                    break;
 
-                    case 6:
-                        actionString = gameActions.category();
-                        paused = true;
-                }
-
-                bot.channels.cache
-                    .get(channelID)
-                    .send(`<@${currPlayer}>, ${actionString}`);
+                case 9:
+                    answer = randomize(8500, 1050);
+                    paused = true;
+                    bot.channels.cache
+                        .get(channelID)
+                        .send(`Type the number "${answer}" in chat.`);
+                    break;
             }
         }
-    }, 25000);
+    }, 5000);
 }
 
 module.exports.addUser = function(msgObj) {
@@ -105,6 +120,8 @@ module.exports.stop = () => {
 
     if (started) {
         started = false;
+        paused = false;
+        answer = null;
         players = [];
         channelID = '';
         idInterval = null;
@@ -118,6 +135,7 @@ module.exports.stop = () => {
 module.exports.resume = bot => {
     if (paused) {
         paused = false;
+        answer = null;
         bot.channels.cache.get(channelID).send(`The game have resumed.`);
         return true;
     } else {
@@ -126,13 +144,9 @@ module.exports.resume = bot => {
 };
 
 module.exports.tryAnswer = (pAnswer, msgObj) => {
-    console.log('test');
-    console.log(answer);
-    console.log(pAnswer);
-
-    if (answer == pAnswer) {
+    if (answer !== null && answer == pAnswer) {
         paused = false;
-        answer = 0;
+        answer = null;
         msgObj.reply(` you won, give ${randomize(3, 1)} sips`);
     }
 };
